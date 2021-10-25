1fff87f255c1fe6f7ede475416e41e5dcbaef80d
villefort <ethanmross92@gmail.com>
2021-10-25 19:29:13 -0400

    Some cli work

--- sortz/__main__.py
+++ sortz/__main__.py
@@ -6,19 +6,18 @@ from .sorters import bubble_sort, selection_sort
 def main():
 
     description = \
-    f"""
+f"""
 A program to visualze various sorting algorithms.
 
 example usage:
-python {__file__} --sorter bubble --fps 30
-    """
-
-    parser = argparse.ArgumentParser(prog = f"python {__file__}", description = description,
+sortz-cli --fps 60 bubble\
+"""
+    parser = argparse.ArgumentParser(prog = f"sortz-cli", description = description,
                                      formatter_class = argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("--sorter", help = "sorting algorithm",
-                        choices = ["bubble", "selection"], required = True)
     parser.add_argument("--fps", type = int, help = "fps of visualizer",
-                        choices = range(1, 121), metavar = "{1-120}", required = True)
+                        choices = range(1, 121), metavar = "{1-120}")
+    parser.add_argument("sorter", help = "sorting algorithm",
+                        choices = ["bubble", "selection"])
 
     if len(sys.argv) == 1:
         parser.print_help(sys.stdout)
