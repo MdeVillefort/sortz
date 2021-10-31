a4c86dfc01c6f27c061c6e0349fa884c178a210e
villefort <ethanmross92@gmail.com>
2021-10-31 19:27:47 -0400

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 REQUIREMENTS = (HERE / "requirements.txt").read_text()
 REQUIREMENTS = REQUIREMENTS.split('\n')
 
